422a33ef3cda544ac1805ae0b95ca1e2e1ca4df4
jtw <jtw@soulenergy.co.kr>
2023-09-15 01:21:10 +0000

    ba

--- gitpush.sh
+++ gitpush.sh
@@ -1,4 +1,4 @@
 cd ~/web-service/api/simple-python-pyinstaller-app
 git add .
-git commit -m "asd"
+git commit -m "ba$1"
 git push
--- src/main.py
+++ src/main.py
@@ -1,5 +1,14 @@
 
 import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World123123123456456"}
+
 
 if __name__ == "__main__":
-    uvicorn.run( "app:app" ,host="0.0.0.0", port=13000,  reload=False, workers=5) 
+    uvicorn.run( app ,host="0.0.0.0", port=13000, reload=False) 
