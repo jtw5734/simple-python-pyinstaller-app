73fe63dee9101c4afb1ce4f006f1b7e3551d4717
jtw <jtw@soulenergy.co.kr>
2023-09-15 16:51:15 +0900

    ba

--- gitpush.sh
+++ gitpush.sh
@@ -2,6 +2,6 @@ VERSION=$(cat VERSION)
 echo $VERSION
 cd ~/web-service/api/simple-python-pyinstaller-app
 git add .
-git commit -m "ba$1"
-git tag -a "$VERSION"
+git commit -m "ba"
+git tag "$VERSION"
 git push
